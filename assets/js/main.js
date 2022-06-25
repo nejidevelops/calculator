52a650517346c5d0c03b634cb9c34ccee8e89e12
NewtonOmbese <newtonombese1@gmail.com>
2022-06-25 08:22:26 +0300

    figuring out my solution

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+// *** Write your code here***
+document.addEventListener('DOMContentLoaded', ()=>{
+    // calculatorApp()
+})
+
+let display = document.querySelector("#displayNumber")
+function calcValues(x){
+        display.value = display.value + x;
+        console.log(x)
+    }
+function calculatorApp(){
+    
+}
+//  Was to imply this method but I had evverything simplified in my html file
+function clear(){
+    form.reset()
+}
+
+function del(){
+
+}
+
+function appendNumber(number){
+
+}
+
+function chooseOperation(operation){
+
+}
+
+function compute(){
+    function sum(num1, num2){
+        return num1 + num2
+    }
+
+    function sub(num1, num2){
+        return num1 - num2
+    }
+
+    function mul(num1, num2){
+        return num1 * num2
+    }
+
+    function div(num1, num2){
+        return num1 / num2
+    }
+}
+
+function updateDisplay(){
+    
+}
+
