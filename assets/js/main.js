9e508f8bc9c12cf45534a163eb59fad36c3d171d
NewtonOmbese <newtonombese1@gmail.com>
2022-06-25 09:20:27 +0300

    phewww\! done at last

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,44 +14,10 @@ clearNum.addEventListener('click', ()=>{
     }
 })
 
-
-
-//  Was to imply this method but I had evverything simplified in my html file
-// function clear(){
-//     form.reset()
-// }
-
-// function del(){
-
-// }
-
-// function appendNumber(number){
-
-// }
-
-// function chooseOperation(operation){
-
-// }
-
-// function compute(){
-//     function sum(num1, num2){
-//         return num1 + num2
-//     }
-
-//     function sub(num1, num2){
-//         return num1 - num2
-//     }
-
-//     function mul(num1, num2){
-//         return num1 * num2
-//     }
-
-//     function div(num1, num2){
-//         return num1 / num2
-//     }
-// }
-
-// function updateDisplay(){
-    
-// }
-
+function deleteNum(){
+    if("del"){
+        let arr=display.value.split("")
+        arr.pop()
+        display.value=arr.join("")
+    }
+}
